8579aa20b129f28966073cf57241622816a73a9c
Antony Meyn <anm@umbraco.dk>
2025-02-07 11:17:07 +0100

    works

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using GeoCoordinateStuff;
+﻿using POC_GeoCoordinate;
 using System.Device.Location;
 
 double latitude;
---
+++ StuffForEric.cs
@@ -0,0 +1,13 @@
+﻿using System.Device.Location;
+
+namespace POC_GeoCoordinate
+{
+	public static class StuffForEric
+	{
+		public static bool IsValid(double latitude, double longitude) // Make this an API endpoint to validate coordinates
+		{
+			try { _ = new GeoCoordinate(latitude, longitude); } catch { return false; }
+			return true;
+		}
+	}
+}
