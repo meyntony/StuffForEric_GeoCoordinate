a61c1eb27efc892f6b6883a58ae72a55160bae32
Antony Meyn <anm@umbraco.dk>
2025-02-07 11:11:18 +0100

    backend

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using GeoCoordinateStuff;
+using System.Device.Location;
+
+double latitude;
+double longitude; 
+bool isValid = true;
+
+do {
+	if (!isValid)
+	{
+		Console.WriteLine("Invalid latitude and longitude. Please try again.");
+	}
+	Console.WriteLine("Enter latitude:");
+	latitude = double.TryParse(Console.ReadLine(), out latitude) ? latitude : -100;
+	Console.WriteLine("Enter longitude:");
+	longitude =  double.TryParse(Console.ReadLine(), out longitude) ? longitude : -100;
+	isValid = StuffForEric.IsValid(latitude, longitude);
+
+} while (!isValid);
+
+var x = new GeoCoordinate(latitude, longitude);
+
+Console.WriteLine($"Latitude: {x.Latitude}, Longitude: {x.Longitude}");
+
+var distance = x.GetDistanceTo(new GeoCoordinate(0, 0));
+
+Console.WriteLine($"Distance to 0,0: {distance} meters");
